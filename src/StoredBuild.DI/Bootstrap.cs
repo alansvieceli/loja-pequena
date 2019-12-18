d60872fa91c36d759f9e198c674437868674d23c
Alan Vieceli <alansvieceli@gmail.com>
2019-12-17 23:22:33 -0300

    Criando um projeto que será responsável pela injeção de dependencia do sistema inteiro

---
+++ src/StoredBuild.DI/Bootstrap.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.EntityFrameworkCore;
+using StoredBuild.Data;
+
+namespace StoredBuild.DI
+{
+    public class Bootstrap
+    {
+        public static void Configure(IServiceCollection services, string strConnection)
+        {
+            //Responsável por toda injeção de dependencia
+            services.AddDbContext<ApplicationDbContext>(options => options.UseMySql(strConnection));
+
+        }
+
+    }
+}
