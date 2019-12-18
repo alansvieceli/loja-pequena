460a2759f087552a8575a1fde703618ee24fede5
Alan Vieceli <alansvieceli@gmail.com>
2019-12-17 22:09:43 -0300

    Ajustes

--- src/StoredBuild.Domain/Interfaces/IRepository.cs
+++ src/StoredBuild.Domain/Interfaces/IRepository.cs
@@ -3,6 +3,6 @@ namespace StoredBuild.Domain.Interfaces
   public interface IRepository<TEntity>
   {
     TEntity GetById(int id);
-    void Save(TEntity entity)
+    void Save(TEntity entity);
   }
 }
